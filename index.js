a3470bd37f912fea4b2043170871a815b12fe909
Andrey Mazépas <andreymazepas@gmail.com>
2020-06-21 23:48:12 -0300

    decided to rewrite everything as simply as possible and then iterate

--- index.js
+++ index.js
@@ -8,42 +8,57 @@
 // Keyboard:	16 keys, organized into 4 rows by 4 columns
 
 
-class Memory {
-    constructor() {
-        this.memory = new Uint8Array(0x1000008);
-    }
+// class Memory {
+//     constructor() {
+//         this.memory = new Uint8Array(0x1000008);
+//     }
 
-    fetch24(addr) {
-        return this.memory[addr]<<16 | this.memory[addr+1]<<8 | this.memory[addry+2];
-    }
+//     fetch24(addr) {
+//         return this.memory[addr]<<16 | this.memory[addr+1]<<8 | this.memory[addry+2];
+//     }
 
-    set24() {
+//     set24() {
         
-    }
+//     }
 
-    getPC() {
-        return this.fetch24(2);
-    }
+//     getPC() {
+//         return this.fetch24(2);
+//     }
 
-    setPC() {
+//     setPC() {
 
-    }
-}
+//     }
+// }
 
-class CPU {
-    constructor(memory) {
-        this.memory = memory;
-        this.pc = memory.getPC();
-    }
+// class CPU {
+//     constructor(memory) {
+//         this.memory = memory;
+//         this.pc = memory.getPC();
+//     }
 
-    fetch() {
-        this.pc = memory.getPC();
-        return ins = this.memory[pc];
-    }
+//     fetch() {
+//         this.pc = memory.getPC();
+//         return ins = this.memory[pc];
+//     }
 
-    execute(ins) {
+//     execute(ins) {
 
-    }
-}
+//     }
+// }
 
+const fileSelector = document.getElementById('file-selector');
+const ctx = document.getElementById('canvas').getContext("2d");
+const imgData = ctx.getImageData(0,0,256, 256);
+
+fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+    const reader = new FileReader();
+    reader.addEventListener('load', (e) => {
+        console.log(e.target.result);
+        imgData.data.set(new Uint8ClampedArray(e.target.result));
+        ctx.putImageData(imgData,0,0);
+    });
+    reader.readAsArrayBuffer(fileList[0]);
+});
 
